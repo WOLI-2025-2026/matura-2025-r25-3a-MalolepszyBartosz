f759c5e3a75a305b92f0c85863a04c8e5180d7a5
MalolepszyBartosz <bartoszmalolepszy8@gmail.com>
2025-11-13 09:07:48 +0000

    c

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,51 @@
 //Bartosz Malolepszy
 
 #include <iostream>
+#include <fstream>
 
 using namespace std;
-int main() {
-    string z3_1_path_symbole_txt = "../zalaczniki-2025/symbole.txt";
 
-    void Zadanie2_1(){
-        ifstream plik(z2_1_path_symbole_txt);
-        
+string z3_1_path_symbole_txt = "../zalaczniki-2025/symbole.txt";
+
+bool czy_palindrom(string slowo)
+{
+    int dlugosc = slowo.length();
+    for(int i = 0; i < dlugosc / 2; i++)
+    {
+        if(slowo[i] != slowo[dlugosc - i - 1])
+        {
+            return false;
+        }
     }
+    return true;
+}
+
+void zadanie2_1()
+{
+    ifstream plik(z3_1_path_symbole_txt);
+    if(!plik.is_open())
+    {
+        cerr << "nie mozna otworzyc pliku" << endl;
+    }
+
+    string linia;
+    while(plik >> linia)
+    {
+        if (czy_palindrom(linia))
+        {
+            cout << linia << endl;
+        }
+    }
+
+
+    plik.close();
+
+}
+
+
+int main() 
+{
+
+    zadanie2_1();
+    return 0;
 }
